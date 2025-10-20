14c3d60738c82da237fa758d9bdcd2d44a18a6c3
Damir Magzhanov <magzhanov.damir@mail.ru>
2025-10-20 10:56:12 +0300

    fix: universal path resolution for UI views

--- src/ui/ui.controller.ts
+++ src/ui/ui.controller.ts
@@ -1,10 +1,29 @@
 import { Controller, Get, Res } from "@nestjs/common";
-import path from "path";
+import { join } from "path";
 
 @Controller()
 export class UiController {
-  @Get(["/", "/login"]) login(@Res() res: any) { res.sendFile(path.join(process.cwd(), "src/ui/views/login.html")); }
-  @Get("/app") app(@Res() res: any) { res.sendFile(path.join(process.cwd(), "src/ui/views/app.html")); }
-  @Get("/admin") admin(@Res() res: any) { res.sendFile(path.join(process.cwd(), "src/ui/views/admin.html")); }
-  @Get("/static/styles.css") css(@Res() res: any) { res.type("text/css").send("body{font-family:sans-serif} .app{display:flex;gap:16px} aside{width:280px}"); }
+  private readonly viewsRoot = join(__dirname, "ui", "views");
+
+  @Get(["/", "/login"])
+  login(@Res() res: any) {
+    res.sendFile("login.html", { root: this.viewsRoot });
+  }
+
+  @Get("/app")
+  app(@Res() res: any) {
+    res.sendFile("app.html", { root: this.viewsRoot });
+  }
+
+  @Get("/admin")
+  admin(@Res() res: any) {
+    res.sendFile("admin.html", { root: this.viewsRoot });
+  }
+
+  @Get("/static/styles.css")
+  css(@Res() res: any) {
+    res
+      .type("text/css")
+      .send("body{font-family:sans-serif} .app{display:flex;gap:16px} aside{width:280px}");
+  }
 }
